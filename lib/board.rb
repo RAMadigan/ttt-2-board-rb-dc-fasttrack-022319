077f2d8626b0f6e65f980671abfcf709f9bb6254
Rich Peebles Madigan <richmadigan5@gmail.com>
2019-02-24 17:43:13 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
-TopRow = [" ", " ", " "]
-MidRow = [" ", " ", " "]
-BotRow = [" ", " ", " "]
-board = TopRow + MidRow + BotRow
+top_row = [" ", " ", " "]
+mid_row = [" ", " ", " "]
+bot_row = [" ", " ", " "]
+board = top_row + mid_row + bot_row
